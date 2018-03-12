4971419d4d0b0951019c05017cc45a4b22d82406
osinmv <31288523+osinmv@users.noreply.github.com>
2018-03-12 13:09:00 -0400

    PerlinNoise

---
+++ A&M_Game/Assets/Scripts/GameMAnager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMAnager : MonoBehaviour {
+
+	private int width = 100;
+	private int height = 100;
+	private float scale = 5;
+	private int moveX = Random.Range (0, 1000);
+	private int moveY = Random.Range (0, 1000);
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+
+
+
+	private float[,] GenerateNoise ()
+	{
+		float [,] a = new float[width,height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				a [i, j] = Mathf.PerlinNoise ((float)i*scale/width+moveX, (float)j*scale/height+moveY);
+
+			}
+		}
+
+		return a;
+	}
+}

6bdea8c565709b0205a78db7407d3be957835af8
osinmv <31288523+osinmv@users.noreply.github.com>
2018-03-16 17:36:53 -0400

    123

---
+++ -Name-objective/A&M_Game/Assets/SkillStructure.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person
+{
+	private bool gender;
+	private List<SkillStructure> skills = new List<SkillStructure>();
+
+
+	public Person(List<SkillStructure> skills)
+	{
+		if (Random.Range (0, 1) == 0) {
+			this.gender = true;
+		} else {
+			this.gender = false;
+		}	
+		this.skills = skills;
+	}
+	public bool Gender
+	{
+		get{ return gender;}
+
+	}
+	public List<SkillStructure> Skills
+	{
+		get{ return skills;}
+
+	}
+}
+public class SkillStructure {
+
+	private string Skillname;
+	private int profecionalism;
+
+	public SkillStructure(string name)
+	{
+		this.Skillname = name;
+		this.profecionalism = Random.Range (0, 10);
+
+	}
+	public int Profecionalism
+	{
+		get{ return profecionalism;}
+
+	}
+}
